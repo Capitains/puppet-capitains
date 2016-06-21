6fdbcca989f7881b8a3acb61f1c2f0ca6bbaf3e8
Bridget Almas <balmas@gmail.com>
2016-06-20 22:22:19 -0400

    upgrading to 1.0.0b

--- manifests/assets.pp
+++
@@ -1,7 +0,0 @@
-#UI Assets for Capitains
-class capitains::assets {
-  exec { 'clone assets':
-    command => "/usr/bin/git clone ${capitains::assets_source} ${capitains::static_root}",
-    creates => $capitains::static_root,
-  }
-}
