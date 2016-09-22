030c68bb1e08678b247fe2b2c745bcd1fd163e5c
Bridget Almas <balmas@gmail.com>
2016-09-22 06:56:55 -0400

    restarts gunicorn on venv update and latest version of apps

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,13 @@ class capitains($www_root,
   include capitains::dependencies
   include capitains::repos
 
-  file { $app_root:
+  file { $capitains::app_root:
     ensure => directory,
     owner  => 'www-data',
     group  => 'www-data',
   }
 
-  file { $data_root:
+  file { $capitains::data_root:
     ensure => directory,
     owner  => 'www-data',
     group  => 'www-data',
@@ -25,36 +25,46 @@ class capitains($www_root,
 
   file { "${app_root}/requirements-py3.txt":
     content => template('capitains/requirements-py3.txt.erb'),
+    notify  => Python::Virtualenv[$capitains::app_root],
   }
 
   file { "${app_root}/manager.py":
     content => template('capitains/manager.py.erb'),
+    notify  => Python::Virtualenv[$capitains::app_root],
   }
 
   file { "${app_root}/app.py":
     content => template('capitains/app.py.erb'),
+    notify  => Python::Virtualenv[$capitains::app_root],
   }
 
   file { "${app_root}/hookclean.py":
     content => template('capitains/hookclean.py.erb'),
   }
 
-  python::virtualenv { $app_root:
+  python::virtualenv { $capitains::app_root:
     ensure       => present,
     require      => Class['capitains::dependencies'],
     version      => '3',
-    requirements => "${app_root}/requirements-py3.txt",
-    venv_dir     => $venvdir,
-    cwd          => $app_root,
+    requirements => "${capitains::app_root}/requirements-py3.txt",
+    venv_dir     => $capitains::venvdir,
+    cwd          => $capitains::app_root,
+    notify       => Exec['restart-gunicorn'],
   }
 
   python::gunicorn { 'vhost':
     ensure     => present,
-    virtualenv => $venvdir,
-    dir        => $app_root,
+    virtualenv => $capitains::venvdir,
+    dir        => $capitains::app_root,
     bind       => 'localhost:5000',
     appmodule  => 'app:app',
     owner      => 'www-data',
     group      => 'www-data',
+  }
+
+  exec { 'restart-gunicorn':
+    command     => '/usr/sbin/service gunicorn restart',
+    refreshonly => true,
+    require     => Python::Gunicorn['vhost'],
   }
 }
